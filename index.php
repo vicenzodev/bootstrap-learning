34ab0f2f749d4bf917c37c3e5d96f513a1691371
Pixel2Play <heitor@vicenzo.dev>
2023-11-24 15:53:35 -0300

    small changes

--- index.php
+++ index.php
@@ -8,7 +8,21 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-	<h1>Hello World</h1>
+	<div class="container">
+	    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+	      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+	        <span class="fs-4">Bootstrap Learning</span>
+	      </a>
+
+	      <ul class="nav nav-pills">
+	        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+	        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+	        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+	        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+	        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+	      </ul>
+	    </header>
+	  </div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
